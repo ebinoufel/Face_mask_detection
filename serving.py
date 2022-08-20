eaad071173f4ccd4974b813856b35a329fa67660
Ebin <noufelebin@gmail.com>
2022-08-20 22:32:22 +0530

    added result

--- serving.py
+++ serving.py
@@ -9,12 +9,20 @@ app.secret_key = os.urandom(25)
 @app.route('/',methods=['GET','POST'])
 
 def index():
+    if request.method == "POST":
+        f = request.files['file']
+        print(f)
+        print(f)
 
-    return render_template("test.html")
-   
-    f = request.files['file']
-    print(f)
-    print('asdfghj')
+        return redirect(url_for("my_result"))
+    #print('asdfghj')
+    else:
+
+        return render_template("test.html")
+@app.route('/result',methods=['GET','POST'])
+def my_result():
+    return render_template("final.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
